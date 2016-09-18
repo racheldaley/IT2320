6e54035e29e5ab912fc26db6b4aaa8ac47bdc991
racheldaley <rachel.daley95@gmail.com>
2016-09-18 12:12:34 -0400

    IT2320 Assignment 1 Website

---
+++ IT2320_Assignment1/js/trivia.js
@@ -0,0 +1,21 @@
+//functions for the trivia games
+function trivia1()
+{
+	var answer1 = document.getElementsByClassName("answer1");
+	answer1[0].innerHTML="Captain Christopher Pike!";
+}
+function trivia1b()
+{
+	var answer2 = document.getElementsByClassName("answer2"); 
+	answer2[0].innerHTML="Martin Luther King Junior!";
+}
+function trivia1c()
+{
+	var answer3 = document.getElementsByClassName("answer3");
+	answer3 [0].innerHTML="Lucille Ball from I Love Lucy";
+}
+function trivia2(x)
+{
+	alert(x);
+}
+
